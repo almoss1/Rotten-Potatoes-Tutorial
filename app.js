09c8db4fd3b4fed48975e5860c26d110a15f5e7e
Alex Mosseri <aljmo98@gmail.com>
2020-05-26 21:43:54 -0400

    Users can see all reviews

--- app.js
+++ app.js
@@ -7,10 +7,19 @@ const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars are Cool!' });
-});
+// app.get('/', (req, res) => {
+//     res.render('home', { msg: 'Handlebars are Cool!' });
+// });
+// OUR MOCK ARRAY OF PROJECTS
+let review = [
+    { title: "Great Review", movieTitle: "Batman II" },
+    { title: "Awesome Movie", movieTitle: "Titanic" }
+]
 
+// INDEX
+app.get('/', (req, res) => {
+    res.render('reviews-index', { reviews: review });
+})
 
 app.listen("3000", () => {
     console.log("App listening on port 3000!'")
